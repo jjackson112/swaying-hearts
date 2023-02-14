bbff1114d80e96f35679dd48e095dbe8eb6429a7
J. Jackson <jjackson11211@gmail.com>
2023-02-14 10:38:08 -0500

    CSS hearts and JS const

---
+++ main.js
@@ -0,0 +1,9 @@
+// theta is for the swaying, floating effect
+// heart object has an "x" and a "y", heart class
+// to make the heart you need 3 divs, a rectangle rotated at 45 degrees with 2 circles on top, appending them to the page
+// setTimeout deletes them after 10 sec so the browser doesn't freeze
+// setInterval creates new hearts and updates the function, making them sway again
+
+
+const hearts = []
+const pinks= ["#ff748c", "#ff8da1", "#ffa7b6"]
